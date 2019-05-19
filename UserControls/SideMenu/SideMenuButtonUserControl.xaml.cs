3e2258e83cf4ba26657a24a68674163f300e3849
Mateusz Piotr Moruś <mpmorus@gmail.com>
2019-05-19 18:01:13 +0200

    Added a simple side-menu template

---
+++ UserControls/SideMenu/SideMenuButtonUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HelloMonitor
+{
+    /// <summary>
+    /// Interaction logic for SideMenuButtonUserControl.xaml
+    /// </summary>
+    public partial class SideMenuButtonUserControl : UserControl
+    {
+        public SideMenuButtonUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
